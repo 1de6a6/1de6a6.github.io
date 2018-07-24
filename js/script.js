ff696996cbfd950589e0b26e21a491b48179094a
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 23:25:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,10 +26,22 @@ function getTradedToken(address) {
   });  
 }  
   
+function get(url) {
+  return new Promise((resolve,reject) => {
+    $.get(url, function(body,err) {
+      console.log(body,err);
+      if(!err) {
+        resolve(body);
+      } else {
+          reject(err);
+      }
+    });  
+  });
+}  
 async function loadContract() {
   let blockNumber = await getBlockNumber();
   let url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=" + blockNumber + "&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q"; 
-  console.log(url);
+  console.log(await get(url));
 }
  
 $(document).ready(function() {
