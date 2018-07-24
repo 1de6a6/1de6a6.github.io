051a48c6180058dc3e132b19acd66c327e36627a
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 21:03:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-let url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=6023449&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q";
-$.get(url, function(err,body) {
-   console.log(err,body);
-}); 
+function getBlockNumber(callback) {
+  web3.eth.getBlockNumber(function(err, blockNumber) {
+    if(!err) {
+      callback(blockNumber); 
+    }
+  });
+}  
+
+function getInternalTxs(blockNumber) {
+  let url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=" + blockNumber + "&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q"; 
+  $.get(url, function(err,body) {
+    let internalTxs = JSON.parse(body)['result']; 
+    console.log(internalTxs); 
+  });
+}   
+
+$(document).ready(function() {
+  getBlockNumber(getInternalTxs);
+});  
