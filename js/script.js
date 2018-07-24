117b282f8f0a84a4bfff43d4228b3de64dca239e
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 21:52:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const liquidityContractABI = [{"constant":false,"inputs":[],"name":"reactivate_t
 function getBlockNumber() {
   return new Promise((resolve,reject) => {
     web3.eth.getBlockNumber(function(err, blockNumber) {
-      console.log(err,blockNumber);
       if(!err) {
         resolve(blockNumber); 
       } else {
@@ -17,9 +16,9 @@ function getInternalTxs(blockNumber) {
   let url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=" + blockNumber + "&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q"; 
   return new Promise((resolve,reject) => {
     $.get(url, function(body,err) {
+      console.log(body,err);
       if(!err) {
-        let internalTxs = body['result']; 
-        resolve(internalTxs); 
+        resolve(body['result']); 
       } else {
           reject(err);
       }
@@ -42,9 +41,8 @@ function getTradedToken(address) {
   
 async function loadInternalTxs() {
   let blockNumber = await getBlockNumber();
-  console.log(blockNumber);
   let internalTxs = await getInternalTxs(blockNumber);
-  console.log(blockNumber,internalTxs);
+  console.log(internalTxs);
   internalTxs.forEach(async function(entry) {
     console.log(entry);
     let txs = await getTradedToken(entry);
