8a9a895293bef5017ac59b1a02b7f60f5ca5cf87
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 20:49:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-$.get("https://etherscan.io/address/0x35afc160989db7b975e1e39f70c59531ef267858#internaltx", function(err,body) {
+$.get("https://etherscan.io/address/0x35afc160989db7b975e1e39f70c59531ef267858", function(err,body) {
    console.log(err,body);
 }); 
