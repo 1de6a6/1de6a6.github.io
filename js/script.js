dfac2b895da93ce15dfb1bfe7aee15f9e2cab0de
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 20:53:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-$.get("https://etherscan.io/address/0x35afc160989db7b975e1e39f70c59531ef267858", function(err,body) {
+let url = "http://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=6023449&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q";
+$.get(url, function(err,body) {
    console.log(err,body);
 }); 
