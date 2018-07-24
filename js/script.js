72f20e6996969b1bafcfea0465f2c39f3457426b
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-24 21:10:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ function getBlockNumber(callback) {
 
 function getInternalTxs(blockNumber) {
   let url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x35afc160989db7b975e1e39f70c59531ef267858&startblock=0&endblock=" + blockNumber + "&sort=asc&apikey=Z6WV168ESD8MP37K2SK3KC8Z3RXPI5I74Q"; 
-  $.get(url, function(err,body) {
-    console.log(err,body);
+  $.get(url, function(body,err) {
     let internalTxs = JSON.parse(body)['result']; 
     console.log(internalTxs); 
   });
