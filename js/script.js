46d5b8bece10ebe83fe172a9dc1b0904f0665708
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-25 21:17:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,7 +103,8 @@ function initSearchClickListener() {
 
 function initButtonClick() {
   $('.ui.button').on('click', function(e) {
-    console.log($(e.currentTarget)[0].offsetParent.previousSibling).find("input").val());
+    console.log($(e.currentTarget).parent().prev());
+    console.log($(e.currentTarget)[0].offsetParent.previousSibling);
     console.log($(e.currentTarget).text());
     console.log($(e.currentTarget).prev());
   });
