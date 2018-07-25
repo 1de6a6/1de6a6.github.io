f3dcb549d9b61bced4aaa97944fc7f0f13267f5b
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-25 13:42:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ async function loadContractInformation(arr) {
     let commission = await getCommission(contractAddress);
     commission = (parseInt(commission)/1e18).toString() + " %";
     let admin = await getAdmin();
-    let rowHTML = "<tr><td>" + admin + "</td><td>" + contractAddress + "</td><td>" + + "</td><td>" + commission + "</td><td>" + + "</td></tr>";
+    let rowHTML = "<tr><td>" + admin + "</td><td>" + contractAddress + "</td><td>" + 0 + "</td><td>" + commission + "</td><td>" + 0 + "</td></tr>";
     console.log(rowHTML);
     $(query).append(rowHTML);
   }  
