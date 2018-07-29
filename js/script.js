0250bd51593174ab0474eb1d2c9abf260123d313
1de6a6 <41541579+1de6a6@users.noreply.github.com>
2018-07-29 17:01:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -278,12 +278,12 @@ async function initButtonClick() {
     let tradeAmount, ethAmount;
     tradeType === "Buy" ? (tradeAmount = inputValue * Math.pow(10,tokenDecimals),
     ethAmount = (tradeAmount*ethBalance)/(tokenBalance + tradeAmount),
-    buyPrice = (tokenBalance + tradeAmount) / ethBalance,
+    buyPrice = ((tokenBalance + tradeAmount) / ethBalance)/1e18,
     $('#ethAmount').text(ethAmount.toString()),$('#buyPrice').text("Price: " + buyPrice.toString()),
     await initBuyClickListener({from:userAddress,to:contractAddress,value:parseInt(ethAmount*1e18)}),
     $('.ui.basic.modal.one').modal('show'))
     : (tradeAmount = inputValue * Math.pow(10,tokenDecimals), $('#tokenAmount').text(inputValue.toString()),
-      sellPrice = (tokenBalance + tradeAmount) / ethBalance,
+      sellPrice = ((tokenBalance + tradeAmount) / ethBalance)/1e18,
       $('#sellPrice').text("Price: " + sellPrice.toString()),
        await initSellClickListener({userAddress:userAddress, tradedTokenAddress:tradedTokenAddress,
 			      tradeAmount:tradeAmount, contractAddress:contractAddress, tradeAmount:tradeAmount}),
